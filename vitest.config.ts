7c12024b29f59fbd1e95d41de761116d54641800
Anthony Bonta <zthun.coding@gmail.com>
2025-11-12 20:36:34 -0600

    build: no need to run serially for this repo

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,6 @@ import {
 } from "@zthun/janitor-build-config/vite";
 import { defineConfig } from "vite";
 
-const test = new ZViteTestBuilder().node().runSerially().monorepo().build();
+const test = new ZViteTestBuilder().node().monorepo().build();
 const config = new ZViteConfigBuilder().test(test).build();
 export default defineConfig(config);
